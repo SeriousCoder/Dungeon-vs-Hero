4f5b7ff310d8efea27f92d7e3d4d2113d8e502de
SeriousCoder <0kintol0@gmail.com>
2015-10-11 23:44:32 +0300

    - Added working version with core (kotlin)

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.NNGame;
+import com.mygdx.game.NNGame2;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new NNGame(), config);
+		initialize(new NNGame2(), config);
 	}
 }
