35ad3d5e2b53e55b9831c8879b60a99bc0473ee1
SeriousCoder <0kintol0@gmail.com>
2015-10-18 18:43:58 +0300

    - Added title on main screen

--- core/src/com/mygdx/screen/Screen.kt
+++ core/src/com/mygdx/screen/Screen.kt
@@ -2,13 +2,34 @@ package com.mygdx.Screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.*
 import com.mygdx.AssertLoader
 
 public class DvHScreen (val assert : AssertLoader) : Screen
 {
     private val batcher = SpriteBatch()
+    private val stage   = Stage()
+
+    val font = assert.generateFont("Doux Medium.ttf", 60, Color.WHITE)
+
+    init
+    {
+        val titleStyle = Label.LabelStyle()
+        titleStyle.font = font
+
+        val title = Label("Dungeon vs Hero", titleStyle)
+        
+        title.setPosition(100f, 350f)
+
+        stage.addActor(title)
+
+        Gdx.input.inputProcessor = stage
+    }
+
 
 
     override fun pause() {
@@ -23,7 +44,8 @@ public class DvHScreen (val assert : AssertLoader) : Screen
         Gdx.app.log("Screen", "hided")
     }
 
-    override fun render(delta: Float) {
+    override fun render(delta: Float)
+    {
         Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
@@ -35,6 +57,9 @@ public class DvHScreen (val assert : AssertLoader) : Screen
         batcher.end()
 
         Gdx.app.log("FPS", fps.toString() + "")
+
+        stage.act(delta)
+        stage.draw()
     }
 
     override fun resume() {
